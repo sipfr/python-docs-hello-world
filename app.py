93b2f01bbdf6516cc339bcf4bda4b737898a9990
sipfr <73323193+sipfr@users.noreply.github.com>
2021-06-01 18:39:48 -1000

    slash

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    image_list = [ f for f in os.listdir('/datasets')  ]
+    image_list = [ f for f in os.listdir('datasets/')  ]
     return "Hello, World!! test22, image_list : " + image_list
-    
+
