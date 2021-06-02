305c84a41c7e3cd8dee3e0a609748bc0f874292a
sipfr <73323193+sipfr@users.noreply.github.com>
2021-06-01 18:31:09 -1000

    test mapping

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!! test22"
+    image_list = [ f for f in os.listdir('/datasets')  ]
+    return "Hello, World!! test22, image_list : " + image_list
